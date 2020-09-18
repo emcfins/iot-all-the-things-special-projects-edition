839f202690da6f881554fc65ad8cad60fa0b44ff
Tim Mattison <tim@mattison.org>
2020-09-18 11:46:25 -0400

    Consolidated duplicated code

--- s1e1-totally-lit/hue-lifx-java/src/main/java/com/awslabs/iot_all_the_things/special_projects_edition/totally_lit/hue/HueShared.java
+++ s1e1-totally-lit/hue-lifx-java/src/main/java/com/awslabs/iot_all_the_things/special_projects_edition/totally_lit/hue/HueShared.java
@@ -72,4 +72,80 @@ public class HueShared {
                 .collect(Collectors.toMap(Room::getName,
                         room -> new ArrayList<String>(room.getLights().stream().map(Light::getName).collect(Collectors.toList()))));
     }
+
+    public static List<String> getOriginalLightAndRoomList(String[] args) {
+        return Arrays.stream(args)
+                // Skip the username argument
+                .skip(1)
+                .collect(Collectors.toList());
+    }
+
+    public static List<String> getCaseInsensitiveLightAndRoomList(List<String> originalLightAndRoomList) {
+        return originalLightAndRoomList.stream()
+                // Make all strings lowercase for case insensitive matching and return a list
+                .map(String::toLowerCase)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Light> getLightsByName(List<String> caseInsensitiveLightAndRoomList) throws ExecutionException, InterruptedException {
+        return getLights().stream()
+                // Find all the matching lights
+                .filter(light -> caseInsensitiveLightAndRoomList.contains(light.getName().toLowerCase()))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Light> getLightsByRoomName(List<String> caseInsensitiveLightAndRoomList) throws ExecutionException, InterruptedException {
+        return getRooms().stream()
+                // Find all the matching rooms
+                .filter(room -> caseInsensitiveLightAndRoomList.contains(room.getName().toLowerCase()))
+                // Extract all of the lights from those rooms
+                .map(Room::getLights)
+                // Flatten them into a single stream and grab the entire list
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+
+    public static List<Light> getFullLightListOrExitIfEmpty(List<String> originalLightAndRoomList, List<Light> lightsByName, List<Light> lightsByRoomName) {
+        List<Light> lights = new ArrayList<>();
+        lights.addAll(lightsByName);
+        lights.addAll(lightsByRoomName);
+
+        if (lights.size() == 0) {
+            log.error("No lights or rooms found that matched the specified names [" + String.join(", ", originalLightAndRoomList) + "]");
+            System.exit(1);
+        }
+
+        return lights;
+    }
+
+    public static List<Light> getLightsOrExitIfEmpty(String[] args) throws ExecutionException, InterruptedException {
+        List<String> originalLightAndRoomList = getOriginalLightAndRoomList(args);
+
+        List<String> caseInsensitiveLightAndRoomList = getCaseInsensitiveLightAndRoomList(originalLightAndRoomList);
+
+        List<Light> lightsByName = getLightsByName(caseInsensitiveLightAndRoomList);
+
+        List<Light> lightsByRoomName = getLightsByRoomName(caseInsensitiveLightAndRoomList);
+
+        List<Light> lights = getFullLightListOrExitIfEmpty(originalLightAndRoomList, lightsByName, lightsByRoomName);
+        return lights;
+    }
+
+    public static void exitAndShowUsageIfNoArgs(String[] args, String scriptName) {
+        if (args.length < 2) {
+            log.error("You must specify the following parameters (in order):");
+            log.error("  - The Hue bridge's username/API key");
+            log.error("  - The Hue bridge's light names and/or room names (in quotes if the name contains spaces)");
+            log.error("");
+            log.error("Example: ./" + scriptName + " USERNAME \"Light 1\" \"Light 2\" Basement");
+            log.error("");
+            log.error("You must specify at least one light but may specify any number of lights by adding more parameters");
+            log.error("");
+            log.error("NOTE: The Hue bridge's IP address is automatically discovered");
+            log.error("NOTE: Light names and room names can be mixed");
+            log.error("NOTE: Light names and room names are case-insensitive");
+
+            System.exit(1);
+        }
+    }
 }
