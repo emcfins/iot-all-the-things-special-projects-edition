79ed74f218f677c85bdcd6df90bfa60a40b701bd
Tim Mattison <tim@mattison.org>
2020-09-18 10:26:33 -0400

    Removed bridge testing run.sh script

--- s1e1-totally-lit/hue-lifx-java/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "If you have not tried this before it may take a minute or two to build the container, subsequent runs will be faster"
-docker build -t hue-lifx-java .
-docker run --network=bridge --rm hue-lifx-java
-docker run --network=host --rm hue-lifx-java
