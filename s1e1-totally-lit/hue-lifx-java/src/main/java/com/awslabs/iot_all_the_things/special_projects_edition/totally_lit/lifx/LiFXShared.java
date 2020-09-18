8177241dc3035c796ed724fc55f727c4519ca036
Tim Mattison <tim@mattison.org>
2020-09-18 12:04:29 -0400

    Added check for Docker in LiFX

--- s1e1-totally-lit/hue-lifx-java/src/main/java/com/awslabs/iot_all_the_things/special_projects_edition/totally_lit/lifx/LiFXShared.java
+++ s1e1-totally-lit/hue-lifx-java/src/main/java/com/awslabs/iot_all_the_things/special_projects_edition/totally_lit/lifx/LiFXShared.java
@@ -6,7 +6,8 @@ import io.vavr.control.Try;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
+import java.io.File;
+import java.nio.file.Files;
 import java.util.Iterator;
 import java.util.Spliterator;
 import java.util.Spliterators;
@@ -18,9 +19,17 @@ public class LiFXShared {
     public static final LFXClient client = new LFXClient();
 
     public static void initializeLiFXClient() {
+        if (isRunningInDocker()) {
+            log.error("LiFX support requires access to the local network to send and receive broadcast payloads.");
+            log.error("Currently we do not know a reliable way to do this in Docker.");
+            log.error("");
+            log.error("If you do please let us know via a Github issue or PR!");
+            System.exit(1);
+        }
         // Open a blocking LiFX client and fail immediately if this blows up
         Try.run(() -> client.open(true)).get();
     }
+
     public static void requireAtLeastOneLight() {
         // Make sure there are some lights. If not give the user some hints on what might be wrong.
         if (getLightCount() == 0) {
@@ -38,5 +47,15 @@ public class LiFXShared {
     public static Stream<LFXLight> getLightStream() {
         Iterator<LFXLight> lightIterator = client.getLights().iterator();
         return StreamSupport.stream(Spliterators.spliteratorUnknownSize(lightIterator, Spliterator.ORDERED), false);
+    }
+
+    public static boolean isRunningInDocker() {
+        String proc1CgroupContents = Try.of(() -> readFileAsString(new File("/proc/1/cgroup"))).getOrElse("");
+
+        return proc1CgroupContents.contains(":/docker/");
+    }
+
+    public static String readFileAsString(File file) {
+        return new String(Try.of(() -> Files.readAllBytes(file.toPath())).get());
     }
 }
